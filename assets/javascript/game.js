f48c4f918af36cada2d9f1d97c0b0d345c1c2198
Leo Orellana <leoorellana@lawn-143-215-62-170.lawn.gatech.edu>
2018-03-01 18:22:02 -0500

    update

---
+++ assets/javascript/game.js
@@ -0,0 +1,65 @@
+
+// here we ask user for his/her name
+var userName = prompt("What is your name?");
+
+// Alerts the output from our confirm game.
+if (userName){
+    alert("Good luck " + userName + "!");
+}
+
+
+    var wins = 0;
+    var losses = 0;
+    var GuessesLeft = 9;
+    var lettersGuessed = [];
+
+    var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y" ,"z"];
+
+
+    // Randomly chooses a choice from the options array. This is the Computer's guess.
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+    console.log(computerGuess);
+    // Next, we give JavaScript a function to execute when onkeyup event fires.
+    document.onkeyup = function(event) {
+
+
+    //determines the key and puts it in lower case and assign user guess.
+    var userGuess = event.key.toLowerCase();
+        console.log(userGuess);
+
+    //logic to determine correct guess.
+        if(event.keyCode<=90 && event.keyCode>=65){
+            if (userGuess === computerGuess){
+                wins++;
+                GuessesLeft = 9;
+                alert("You won bubba!");
+                computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+                document.getElementById("Wins").innerText = wins;
+                document.getElementById("Guesses").innerText= GuessesLeft;
+                document.getElementById("YourGuessesSoFar").innerText = lettersGuessed;
+            }
+            else {
+                GuessesLeft --;
+                document.getElementById("Guesses").innerText= GuessesLeft;
+
+            }
+
+               if(GuessesLeft == 0){
+                   losses ++;
+                   GuessesLeft = 9;
+                   computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+                   document.getElementById("losses").innerText = losses;
+                   document.getElementById("Guesses").innerText= GuessesLeft;
+                   document.getElementById("YourGuessesSoFar").innerText = lettersGuessed;
+
+               }
+
+
+
+        }
+
+
+};
+
+
